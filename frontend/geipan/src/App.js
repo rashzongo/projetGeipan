1c757ba3b24448c14225a7e0bd0f1ff3d16b6284
DiazGabriel <diaz.gabriel@orange.fr>
2020-01-28 12:28:45 +0100

    Add React Architecture

---
+++ frontend/geipan/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>We know the truth</h1>
+      
+       
+
+
+      </header>
+      <body>
+
+
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
